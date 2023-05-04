9f772274b1731aa8550928337119ff9bc602df12
Denis Gorlov <dengorl1998@gmail.com>
2023-05-04 11:54:36 +0300

    tabs changed on 4 spaces in java classes

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -3,11 +3,11 @@ package hexlet.code;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
-	private Validator v;
-	public Validator() {
-		
-	}
-	public StringSchema string() {
-		return new StringSchema();
-	}
+    private Validator v;
+    public Validator() {
+
+    }
+    public StringSchema string() {
+        return new StringSchema();
+    }
 }
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -6,45 +6,45 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-	Validator v = new Validator();
-	StringSchema schema = v.string();
+    Validator v = new Validator();
+    StringSchema schema = v.string();
 
-	@Test
-	public void testString1() {
-		var expected = true;
-		var actual = schema.isValid("");
-		Assertions.assertEquals(expected, actual);
-	}
-	@Test
-	public void testString2() {
-		var expected = true;
-		var actual = schema.isValid(null);
-		Assertions.assertEquals(expected, actual);
-	}
-	@Test
-	public void testString3() {
-		schema.required();
-		var actual = schema.isValid("");
-		var expected = false;
-		Assertions.assertEquals(expected, actual);
-	}
-	@Test
-	public void testString4() {
-		schema.required();
-		var actual = schema.isValid(null);
-		var expected = false;
-		Assertions.assertEquals(expected, actual);
-	}
-	@Test
-	public void testString5() {
-		schema.required();
-		var actual = schema.isValid("Hexlet");
-		var expected = true;
-		Assertions.assertEquals(expected, actual);
-	}
-	@Test void testString6() {
-		var actual = schema.contains("hex");
-		var expected = true;
-		Assertions.assertEquals(expected, actual);
-	}
+    @Test
+    public void testString1() {
+        var expected = true;
+        var actual = schema.isValid("");
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testString2() {
+        var expected = true;
+        var actual = schema.isValid(null);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testString3() {
+        schema.required();
+        var actual = schema.isValid("");
+        var expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testString4() {
+        schema.required();
+        var actual = schema.isValid(5);
+        var expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testString5() {
+        schema.required();
+        var actual = schema.isValid("Hexlet");
+        var expected = true;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test void testString6() {
+        var actual = schema.contains("hex");
+        var expected = true;
+        Assertions.assertEquals(expected, actual);
+    }
 }
