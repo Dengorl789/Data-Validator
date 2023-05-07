c4bd28e19e9d6c7d8a48af7bfabd85f7c9ccd759
Denis Gorlov <dengorl1998@gmail.com>
2023-05-07 22:26:55 +0300

    StringSchema logic updated

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,25 +1,34 @@
 package hexlet.code.schemas;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
+
 public class StringSchema {
-    private String str;
+    private Map<String, Predicate> limitations = new HashMap<>();
     private boolean state = true;
     public void required() {
-        this.state = Objects.nonNull(this.str) && !this.str.equals("");
+        Predicate<String> req = (s) -> Objects.nonNull(s) && !s.equals("");
+        limitations.put("required", req);
     }
     public void minLength(int minLength) {
-        required();
-        this.state = this.str.length() >= minLength;
+        Predicate<String> min = (s) -> s.length() >= minLength;
+        limitations.put("minLength", min);
     }
-    public boolean contains(String pattern) {
-        return this.state = this.str.contains(pattern);
+    public void contains(String pattern) {
+        Predicate<String> cont = (s) -> s.contains(pattern);
+        limitations.put("contains", cont);
     }
-    public boolean  isValid(String vString) {
-        this.str = vString;
-        required();
-        return state;
-    }
-    public boolean  isValid(int minLength) {
-        required();
-        return this.str.length() >= minLength;
+    public boolean isValid(Object validatedObject) {
+        if (Objects.nonNull(validatedObject)
+                && validatedObject.getClass() != String.class) {
+            return false;
+        }
+        boolean result = true;
+        for (Map.Entry<String, Predicate> predicate : limitations.entrySet()) {
+            result = predicate.getValue().test(validatedObject);
+        }
+        return result;
     }
 }
+
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
-import hexlet.code.Validator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -30,7 +29,6 @@ public class AppTest {
     }
     @Test
     public void testString4() {
-        schema.required();
         var actual = schema.isValid(5);
         var expected = false;
         Assertions.assertEquals(expected, actual);
@@ -38,13 +36,31 @@ public class AppTest {
     @Test
     public void testString5() {
         schema.required();
+        schema.minLength(4);
         var actual = schema.isValid("Hexlet");
         var expected = true;
         Assertions.assertEquals(expected, actual);
     }
-    @Test void testString6() {
-        var actual = schema.contains("hex");
+    @Test
+    public void testString6() {
+        schema.required();
+        schema.minLength(6);
+        var actual = schema.isValid("Hi");
+        var expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testString7() {
+        schema.contains("Hex");
+        var actual = schema.isValid("Hexlet");
         var expected = true;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testString8() {
+        schema.contains("what");
+        var actual = schema.isValid("Hello, World!");
+        var expected = false;
         Assertions.assertEquals(expected, actual);
     }
 }
